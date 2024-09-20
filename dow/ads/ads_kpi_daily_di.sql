9bbdd09d168e46e694882324a62d8e27ddb9953f
Dingyelen <dingyanlin_happy@qq.com>
2024-09-20 18:53:02 +0800

    [优化]dow dws

--- dow/ads/ads_kpi_daily_di.sql
+++ dow/ads/ads_kpi_daily_di.sql
@@ -17,8 +17,8 @@ pay_count_daily bigint,
 money_rmb_daily decimal(36, 2),
 new_users_moneyrmb decimal(36, 2), 
 users_new_moneyrmb decimal(36, 2), 
-new_users_ac bigint,
 moneyrmb_ac decimal(36, 2),
+new_users_ac bigint,
 part_date varchar
 )
 with(partitioned_by = array['part_date']);
@@ -30,11 +30,11 @@ and part_date <= $end_date;
 insert into hive.dow_jpnew_w.ads_kpi_daily_di
 (date, zone_id, channel, os, 
 new_users, active_users, 
-pay_users, paid_users, new_users_pay, users_new_pay,
+pay_users, paid_users, new_users_pay, users_new_pay, 
 online_time, 
-pay_count_daily, money_rmb_daily,
+pay_count_daily, money_rmb_daily, 
 new_users_moneyrmb, users_new_moneyrmb, 
-new_users_ac, moneyrmb_ac,
+web_rmb_daily, web_rmb_users, moneyrmb_ac, newuser_ac, 
 part_date)
 
 with user_daily as(
@@ -44,10 +44,7 @@ role_id,
 level_min as level_min_daily, level_max as level_max_daily,
 viplevel_min as viplevel_min_daily, viplevel_max as viplevel_max_daily, 
 online_time, 
-exchange_rate, 
-pay_count as pay_count_daily, 
-money as money_daily, 
-money_rmb as money_rmb_daily
+exchange_rate, pay_count as pay_count_daily, money as money_daily, money_rmb as money_rmb_daily, web_rmb as web_rmb_daily
 from hive.dow_jpnew_w.dws_user_daily_di 
 where part_date >= $start_date
 and part_date <= $end_date
@@ -60,16 +57,19 @@ a.role_id,
 a.level_min_daily, a.level_max_daily,
 a.viplevel_min_daily, a.viplevel_max_daily, 
 a.online_time, 
-a.exchange_rate, a.pay_count_daily, a.money_daily, a.money_rmb_daily, 
-b.install_date, date(b.lastlogin_ts) as lastlogin_date, 
-b.moneyrmb_ac, b.firstpay_date, b.firstpay_goodid, b.firstpay_level,
-b.zone_id, b.channel, b.os, 
-date_diff('day', b.install_date, a.date) as retention_day,
-date_diff('day', b.firstpay_date, a.date) as pay_retention_day,
-date_diff('day', b.install_date, firstpay_date) as firstpay_interval_days
+a.exchange_rate, a.pay_count_daily, a.money_daily, a.money_rmb_daily, a.web_rmb_daily, 
+b.moneyrmb_ac, b.newuser_ac, 
+c.install_date, date(c.lastlogin_ts) as lastlogin_date, 
+c.firstpay_date, c.firstpay_goodid, c.firstpay_level,
+c.zone_id, c.channel, c.os, 
+date_diff('day', c.install_date, a.date) as retention_day,
+date_diff('day', c.firstpay_date, a.date) as pay_retention_day,
+date_diff('day', c.install_date, firstpay_date) as firstpay_interval_days
 from user_daily a
-left join hive.dow_jpnew_w.dws_user_info_di b
-on a.role_id = b.role_id
+left join hive.dow_jpnew_w.dws_user_daily_derive_df b
+on a.role_id = b.role_id and a.part_date = b.part_date
+left join hive.dow_jpnew_w.dws_user_info_di c
+on a.role_id = c.role_id
 where b.is_test is null
 ),
 
@@ -85,20 +85,18 @@ sum(online_time) as online_time,
 sum(pay_count_daily) as pay_count_daily, 
 sum(money_rmb_daily) as money_rmb_daily, 
 sum(case when money_daily > 0 and retention_day = 0 and pay_retention_day = 0 then money_rmb_daily else null end) as new_users_moneyrmb, 
-sum(case when money_daily > 0 and pay_retention_day = 0 then money_rmb_daily else null end) as users_new_moneyrmb
+sum(case when money_daily > 0 and pay_retention_day = 0 then money_rmb_daily else null end) as users_new_moneyrmb, 
+sum(web_rmb_daily) as web_rmb_daily, 
+count(distinct (case when web_rmb_daily > 0  then role_id else null end)) as web_rmb_users, 
+sum(moneyrmb_ac) as moneyrmb_ac, 
+sum(newuser_ac) as newuser_ac
 from user_daily_join
 group by 1, 2, 3, 4, 5
 ),
 
 data_cube as
-(select * from
-(select distinct date, part_date from daily_info)
-cross join
-(select distinct zone_id from daily_info)
-cross join
-(select distinct channel from daily_info)
-cross join
-(select distinct os from daily_info)
+(select distinct zone_id, channel, os, date from daily_info
+cross join unnest(sequence($start_date, current_date, interval '1' day)) as t(date)
 ),
 
 daily_info_cube as
@@ -107,8 +105,7 @@ b.new_users, b.active_users,
 b.pay_users, b.paid_users, b.new_users_pay, b.users_new_pay, 
 b.online_time, 
 b.pay_count_daily, b.money_rmb_daily, b.new_users_moneyrmb, b.users_new_moneyrmb, 
-sum(b.new_users) over (partition by a.zone_id, a.channel, a.os order by a.date rows between unbounded preceding and current row) as new_users_ac,
-sum(b.money_rmb_daily) over (partition by a.zone_id, a.channel, a.os order by a.date rows between unbounded preceding and current row) as moneyrmb_ac
+b.web_rmb_daily, b.web_rmb_users, b.moneyrmb_ac, b.newuser_ac
 from data_cube a
 left join daily_info b
 on a.date = b.date 
@@ -123,7 +120,7 @@ pay_users, paid_users, new_users_pay, users_new_pay,
 online_time, 
 pay_count_daily, money_rmb_daily, 
 new_users_moneyrmb, users_new_moneyrmb, 
-new_users_ac, moneyrmb_ac,
+web_rmb_daily, web_rmb_users, moneyrmb_ac, newuser_ac, 
 part_date
 from daily_info_cube
 ;
--- dow/dws/dws_user_daily_derive_df.sql
+++ dow/dws/dws_user_daily_derive_df.sql
@@ -8,12 +8,15 @@ login_days bigint,
 is_firstpay bigint, 
 is_pay bigint, 
 is_paid bigint,
-money_ac bigint, 
-moneyrmb_ac bigint,
+is_new bigint,
+money_ac decimal(36, 2), 
+moneyrmb_ac decimal(36, 2),
+webrmb_ac decimal(36, 2), 
 sincetimes_end bigint, 
 core_end bigint, 
 free_end bigint, 
 paid_end bigint, 
+newuser_ac bigint, 
 before_date date, 
 after_date date, 
 part_date varchar
@@ -21,17 +24,17 @@ part_date varchar
 
 insert into hive.dow_jpnew_w.dws_user_daily_derive_df(
 date, role_id, login_days, 
-is_firstpay, is_pay, is_paid,
-money_ac, moneyrmb_ac,
+is_firstpay, is_pay, is_paid, is_new, 
+money_ac, moneyrmb_ac, webrmb_ac, 
 sincetimes_end, core_end, free_end, paid_end, 
-before_date, after_date, 
+newuser_ac, before_date, after_date, 
 part_date
 )
 
 with user_daily as(
 select date, role_id, 
 row_number() over(partition by role_id order by part_date) as login_days, 
-firstpay_ts, money, money_rmb, 
+firstpay_ts, money, money_rmb, web_rmb, 
 sincetimes_end, core_end, free_end, paid_end, 
 part_date
 from hive.dow_jpnew_w.dws_user_daily_di
@@ -40,7 +43,7 @@ from hive.dow_jpnew_w.dws_user_daily_di
 daily_log as(
 select a.date, a.role_id, 
 b.install_date, 
-a.login_days, a.firstpay_ts, a.money, a.money_rmb, 
+a.login_days, a.firstpay_ts, a.money, a.money_rmb, a.web_rmb,
 a.sincetimes_end, a.core_end, a.free_end, a.paid_end, 
 a.part_date
 from user_daily a
@@ -54,6 +57,7 @@ money, money_rmb,
 min(firstpay_ts) over(partition by role_id order by part_date rows between unbounded preceding and unbounded following) as firstpay_ts, 
 sum(money) over(partition by role_id order by part_date rows between unbounded preceding and current row) as money_ac, 
 sum(money_rmb) over(partition by role_id order by part_date rows between unbounded preceding and current row) as moneyrmb_ac, 
+sum(web_rmb) over(partition by role_id order by part_date rows between unbounded preceding and current row) as webrmb_ac, 
 last_value(sincetimes_end) ignore nulls over(partition by role_id order by part_date rows between unbounded preceding and current row) as sincetimes_end, 
 last_value(core_end) ignore nulls over(partition by role_id order by part_date rows between unbounded preceding and current row) as core_end, 
 last_value(free_end) ignore nulls over(partition by role_id order by part_date rows between unbounded preceding and current row) as free_end, 
@@ -66,6 +70,7 @@ from daily_log
 
 daily_boolean_cal as(
 select date, role_id, login_days, 
+(case when date = install_date then 1 else 0 end) as is_new, 
 (case when date(firstpay_ts) = date(part_date) then 1 else 0 end) as is_firstpay, 
 (case when money > 0 then 1 else 0 end) as is_pay, 
 (case when money_ac > 0 then 1 else 0 end) as is_paid, 
@@ -78,9 +83,10 @@ from daily_cal
 
 select
 date, role_id, login_days, 
-is_firstpay, is_pay, is_paid,
-money_ac, moneyrmb_ac,
+is_firstpay, is_pay, is_paid, is_new, 
+money_ac, moneyrmb_ac, webrmb_ac, 
 sincetimes_end, core_end, free_end, paid_end, 
+sum(is_new) over(order by date) as newuser_ac, 
 before_date, after_date, 
 part_date
 from daily_boolean_cal;
