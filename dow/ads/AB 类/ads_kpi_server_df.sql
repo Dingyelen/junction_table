7b14467cd623d6db17b7725700d1b8ad7b076478
Dingyelen <dingyanlin_happy@qq.com>
2024-11-18 15:53:34 +0800

    [优化]server指标

--- dow/ads/AB 类/ads_kpi_server_df.sql
+++ dow/ads/AB 类/ads_kpi_server_df.sql
@@ -5,32 +5,34 @@ zone_id varchar,
 open_date date, 
 yesterday_new_users bigint,
 yesterday_active_users bigint,
-yesterday_money decimal(36, 2), 
+yesterday_moneyrmb decimal(36, 2), 
 new_users bigint,
 active_users bigint,
 money decimal(36, 2),
+money_rmb decimal(36, 2),
 newusers_ac bigint,
 money_ac decimal(36, 2),
+moneyrmb_ac decimal(36, 2),
 leveltop_roleid varchar,
 leveltop_level bigint,
-leveltop_money decimal(36, 2),
+leveltop_moneyrmb decimal(36, 2),
 paytop_roleid varchar,
 paytop_level bigint,
-paytop_money decimal(36, 2)
+paytop_moneyrmb decimal(36, 2)
 );
 
 insert into hive.dow_jpnew_w.ads_kpi_server_df(
 zone_id, open_date, 
-yesterday_new_users, yesterday_active_users, yesterday_money, 
-new_users, active_users, money,
-newusers_ac, money_ac, 
-leveltop_roleid, leveltop_level, leveltop_money, 
-paytop_roleid, paytop_level, paytop_money
+yesterday_new_users, yesterday_active_users, yesterday_moneyrmb, 
+new_users, active_users, money, money_rmb,
+newusers_ac, money_ac, moneyrmb_ac, 
+leveltop_roleid, leveltop_level, leveltop_moneyrmb, 
+paytop_roleid, paytop_level, paytop_moneyrmb
 )
 
 with server_daily as(
 select date, part_date, zone_id, open_date, 
-new_users, active_users, firstpay_users, money, pay_count, newusers_ac, money_ac 
+new_users, active_users, firstpay_users, money, money_rmb, pay_count, newusers_ac, money_ac, moneyrmb_ac
 from hive.dow_jpnew_w.dws_server_daily_df
 where part_date >= date_format(date_add('day', -15, current_date), '%Y-%m-%d')
 and part_date <= date_format(current_date, '%Y-%m-%d')
@@ -41,8 +43,10 @@ select zone_id, open_date,
 sum(new_users) as new_users, 
 sum(active_users) as active_users, 
 sum(money) as money, 
+sum(money_rmb) as money_rmb, 
 max(newusers_ac) as newusers_ac, 
-max(money_ac) as money_ac 
+max(money_ac) as money_ac,  
+sum(moneyrmb_ac) as moneyrmb_ac
 from server_daily
 group by 1, 2
 ), 
@@ -51,7 +55,7 @@ server_yesterday_agg as(
 select zone_id, 
 sum(new_users) as yesterday_new_users, 
 sum(active_users) as yesterday_active_users, 
-sum(money) as yesterday_money
+sum(money_rmb) as yesterday_moneyrmb
 from server_daily
 where part_date = date_format(date_add('day', -1, current_date), '%Y-%m-%d')
 group by 1
@@ -60,7 +64,7 @@ group by 1
 user_info as(
 select 
 zone_id, role_id, 
-level, money_ac, 
+level, moneyrmb_ac, 
 row_number() over(partition by zone_id order by level desc, money_ac desc) as level_desc, 
 row_number() over(partition by zone_id order by money_ac desc, level desc) as money_desc
 from hive.dow_jpnew_w.dws_user_info_di
@@ -68,24 +72,24 @@ from hive.dow_jpnew_w.dws_user_info_di
 
 user_level_select as(
 select zone_id, role_id as leveltop_roleid, 
-level as leveltop_level, money_ac as leveltop_money
+level as leveltop_level, moneyrmb_ac as leveltop_moneyrmb
 from user_info
 where level_desc = 1
 ), 
 
 user_money_select as(
 select zone_id, role_id as paytop_roleid, 
-level as paytop_level, money_ac as paytop_money
+level as paytop_level, moneyrmb_ac as paytop_moneyrmb
 from user_info
 where money_desc = 1
 )
 
 select a.zone_id, a.open_date, 
-b.yesterday_new_users, b.yesterday_active_users, b.yesterday_money, 
-a.new_users, a.active_users, a.money,
-a.newusers_ac, a.money_ac, 
-c.leveltop_roleid, c.leveltop_level, c.leveltop_money, 
-d.paytop_roleid, d.paytop_level, d.paytop_money
+b.yesterday_new_users, b.yesterday_active_users, b.yesterday_moneyrmb, 
+a.new_users, a.active_users, a.money, a.money_rmb,
+a.newusers_ac, a.money_ac, a.moneyrmb_ac, 
+c.leveltop_roleid, c.leveltop_level, c.leveltop_moneyrmb, 
+d.paytop_roleid, d.paytop_level, d.paytop_moneyrmb
 from server_all_agg a
 left join server_yesterday_agg b
 on a.zone_id = b.zone_id
--- dow/dws/AB 类/dws_server_daily_df.sql
+++ dow/dws/AB 类/dws_server_daily_df.sql
@@ -15,6 +15,9 @@ firstdaypay_users bigint,
 money decimal(36, 2),
 app_money decimal(36, 2),
 web_money decimal(36, 2),
+money_rmb decimal(36, 2),
+app_moneyrmb decimal(36, 2),
+web_moneyrmb decimal(36, 2),
 pay_count bigint,
 app_count bigint,
 web_count bigint,
@@ -23,12 +26,13 @@ firstday_pay decimal(36, 2),
 newusers_ac bigint, 
 payusers_ac bigint, 
 money_ac decimal(36, 2), 
+moneyrmb_ac decimal(36, 2), 
 leveltop_roleid varchar,
 leveltop_level bigint,
-leveltop_money decimal(36, 2),
+leveltop_moneyrmb decimal(36, 2),
 paytop_roleid varchar,
 paytop_level bigint,
-paytop_money decimal(36, 2),
+paytop_moneyrmb decimal(36, 2),
 retention_day bigint,
 part_date varchar
 )
@@ -44,18 +48,23 @@ new_users, active_users,
 paid_users, pay_users, web_users, 
 firstpay_users, firstdaypay_users, 
 money, app_money, web_money, 
+money_rmb, app_moneyrmb, web_moneyrmb, 
 pay_count, app_count, web_count, 
 first_pay, firstday_pay, 
-newusers_ac, payusers_ac, money_ac,
-leveltop_roleid, leveltop_level, leveltop_money, 
-paytop_roleid, paytop_level, paytop_money, 
+newusers_ac, payusers_ac, money_ac, moneyrmb_ac, 
+leveltop_roleid, leveltop_level, leveltop_moneyrmb, 
+paytop_roleid, paytop_level, paytop_moneyrmb, 
 retention_day, part_date)
 
 with user_daily as(
 select date, part_date, role_id, 
 level_min, level_max, viplevel_min, viplevel_max, 
-online_time, money, app_money, web_money, pay_count, app_count, web_count
-from hive.dow_jpnew_w.dws_user_daily_di
+online_time, money, app_money, web_money, 
+money * rate as money_rmb, app_money * rate as app_moneyrmb, web_money * rate as web_moneyrmb, 
+pay_count, app_count, web_count
+from hive.dow_jpnew_w.dws_user_daily_di a
+left join mysql_bi_r."gbsp-bi-bigdata".t_currency_rate b
+on a.currency = b.currency and date_format(a.date, '%Y-%m') = b.currency_time 
 ), 
 
 user_daily_derive as(
@@ -70,6 +79,7 @@ a.level_min, a.level_max, a.viplevel_min, a.viplevel_max,
 b.is_new, b.is_firstpay, b.is_pay, b.is_paid, 
 a.online_time, 
 a.money, a.app_money, a.web_money, 
+a.money_rmb, a.app_moneyrmb, a.web_moneyrmb, 
 a.pay_count, a.app_count, a.web_count
 from user_daily a
 left join user_daily_derive b
@@ -91,7 +101,10 @@ sum(is_firstpay) as firstpay_users,
 count(distinct case when is_firstpay = 1 and is_new = 1 then role_id else null end) as firstdaypay_users, 
 sum(money) as money, 
 sum(app_money) as app_money, 
-sum(web_money) as web_money, 
+sum(web_money) as web_money,
+sum(money_rmb) as money_rmb, 
+sum(app_moneyrmb) as app_moneyrmb, 
+sum(web_moneyrmb) as web_moneyrmb, 
 sum(pay_count) as pay_count, 
 sum(app_count) as app_count, 
 sum(web_count) as web_count, 
@@ -103,7 +116,7 @@ group by 1, 2, 3
 
 user_daily_rn as(
 select date, part_date, role_id, 
-zone_id, level_max, money, 
+zone_id, level_max, money, money_rmb, 
 row_number() over(partition by date, zone_id order by level_max desc, money desc) as level_desc, 
 row_number() over(partition by date, zone_id order by money desc, level_max desc) as money_desc
 from user_daily_join
@@ -114,9 +127,10 @@ server_info as
 a.online_time, 
 a.new_users, a.active_users, 
 a.paid_users, a.pay_users, a.web_users, a.firstpay_users, a.firstdaypay_users, 
-a.money, a.app_money, a.web_money, a.pay_count, a.app_count, a.web_count, a.first_pay, a.firstday_pay, 
-b.role_id as leveltop_roleid, b.level_max as leveltop_level, b.money as leveltop_money, 
-c.role_id as paytop_roleid, c.level_max as paytop_level, c.money as paytop_money
+a.money, a.app_money, a.web_money, a.money_rmb, a.app_moneyrmb, a.web_moneyrmb, 
+a.pay_count, a.app_count, a.web_count, a.first_pay, a.firstday_pay, 
+b.role_id as leveltop_roleid, b.level_max as leveltop_level, b.money_rmb as leveltop_moneyrmb, 
+c.role_id as paytop_roleid, c.level_max as paytop_level, c.money_rmb as paytop_moneyrmb
 from daily_info a
 left join user_daily_rn b
 on a.date = b.date and a.zone_id = b.zone_id 
@@ -131,12 +145,14 @@ select date, part_date, zone_id,
 online_time, 
 new_users, active_users, 
 paid_users, pay_users, web_users, firstpay_users, firstdaypay_users, 
-money, app_money, web_money, pay_count, app_count, web_count, first_pay, firstday_pay, 
+money, app_money, web_money, money_rmb, app_moneyrmb, web_moneyrmb, 
+pay_count, app_count, web_count, first_pay, firstday_pay, 
 sum(new_users) over(partition by zone_id order by part_date rows between unbounded preceding and current row) as newusers_ac, 
 sum(firstpay_users) over(partition by zone_id order by part_date rows between unbounded preceding and current row) as payusers_ac, 
 sum(money) over(partition by zone_id order by part_date rows between unbounded preceding and current row) as money_ac, 
-leveltop_roleid, leveltop_level, leveltop_money, 
-paytop_roleid, paytop_level, paytop_money
+sum(money_rmb) over(partition by zone_id order by part_date rows between unbounded preceding and current row) as moneyrmb_ac, 
+leveltop_roleid, leveltop_level, leveltop_moneyrmb, 
+paytop_roleid, paytop_level, paytop_moneyrmb
 from server_info
 ), 
 
@@ -152,11 +168,13 @@ online_time,
 new_users, active_users, 
 paid_users, pay_users, web_users, 
 firstpay_users, firstdaypay_users, 
-money, app_money, web_money, pay_count, app_count, web_count, 
+money, app_money, web_money, 
+money_rmb, app_moneyrmb, web_moneyrmb, 
+pay_count, app_count, web_count, 
 first_pay, firstday_pay, 
-newusers_ac, payusers_ac, money_ac,
-leveltop_roleid, leveltop_level, leveltop_money, 
-paytop_roleid, paytop_level, paytop_money, 
+newusers_ac, payusers_ac, money_ac, moneyrmb_ac, 
+leveltop_roleid, leveltop_level, leveltop_moneyrmb, 
+paytop_roleid, paytop_level, paytop_moneyrmb, 
 part_date
 from server_win_info 
 )
@@ -167,11 +185,12 @@ new_users, active_users,
 paid_users, pay_users, web_users, 
 firstpay_users, firstdaypay_users, 
 money, app_money, web_money, 
+money_rmb, app_moneyrmb, web_moneyrmb, 
 pay_count, app_count, web_count, 
 first_pay, firstday_pay, 
-newusers_ac, payusers_ac, money_ac,
-leveltop_roleid, leveltop_level, leveltop_money, 
-paytop_roleid, paytop_level, paytop_money, 
+newusers_ac, payusers_ac, money_ac, moneyrmb_ac, 
+leveltop_roleid, leveltop_level, leveltop_moneyrmb, 
+paytop_roleid, paytop_level, paytop_moneyrmb, 
 date_diff('day', open_date, date) as retention_day, 
 part_date
 from server_daily_res
